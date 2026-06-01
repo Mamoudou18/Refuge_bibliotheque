d34e0ce4823728025fd36edd2398c0ad2690bcee
Mamoudou18 <mamoudou.traore180422@gmail.com>
2026-06-02 00:06:20 +0200

    Mise des bases du routage

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Catalogue } from './pages/catalogue/catalogue';
+import { Emprunts } from './pages/emprunts/emprunts';
+import { Connexion } from './pages/connexion/connexion';
+import { Inscription } from './pages/inscription/inscription';
+import { MonCompte } from './pages/mon-compte/mon-compte';
+import { Deconnexion } from './pages/deconnexion/deconnexion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home',pathMatch: 'full'},
+    { path: 'home', component: Home},
+    { path: 'catalogue', component: Catalogue},
+    { path: 'emprunt', component: Emprunts},
+    { path: 'connexion', component: Connexion},
+    { path: 'inscription', component: Inscription},
+    { path: 'mon-compte', component: MonCompte},
+    { path: 'deconnexion', component: Deconnexion}
+];
