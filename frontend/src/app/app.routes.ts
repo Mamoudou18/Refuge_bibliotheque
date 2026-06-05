cbf1421fc01d9c087a2e07c35b1009db97b5f723
Mamoudou18 <mamoudou.traore180422@gmail.com>
2026-06-05 19:05:28 +0200

    Mise en place statique de la page Détail d'un livre

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Catalogue } from './pages/catalogue/catalogue';
+import { DetailLivre } from './pages/detail-livre/detail-livre';
 import { Emprunts } from './pages/emprunts/emprunts';
 import { Connexion } from './pages/connexion/connexion';
 import { Inscription } from './pages/inscription/inscription';
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: '', redirectTo: 'home',pathMatch: 'full'},
     { path: 'home', component: Home},
     { path: 'catalogue', component: Catalogue},
+    { path: 'detail-livre', component: DetailLivre},
     { path: 'emprunt', component: Emprunts},
     { path: 'connexion', component: Connexion},
     { path: 'inscription', component: Inscription},
