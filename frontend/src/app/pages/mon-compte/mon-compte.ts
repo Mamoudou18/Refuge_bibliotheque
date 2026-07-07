f4bee6a92defbc56cb0e48e7685bc3135c6df0cf
Mamoudou18 <mamoudou.traore180422@gmail.com>
2026-07-07 21:32:47 +0200

    feature:  page mon compte > definition de la sturcture statique des differentes sections: Gestion des livres, Gestion des membres, ...

--- frontend/src/app/pages/mon-compte/mon-compte.ts
+++ frontend/src/app/pages/mon-compte/mon-compte.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mon-compte',
+  standalone: true,
   imports: [],
   templateUrl: './mon-compte.html',
   styleUrl: './mon-compte.scss',
 })
 export class MonCompte {
-
+  activeSection: string = 'accueil';
+  showSection(section: string): void{
+    this.activeSection = section;
+  }
 }
